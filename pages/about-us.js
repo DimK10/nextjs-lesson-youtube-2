9936f25269be363876ba5be478ed97d7863b779c
DimK10 <james_k23@hotmail.gr>
2022-12-20 22:09:55 +0200

    Lesson at 42:22

---
+++ pages/about-us.js
@@ -0,0 +1,18 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Abous us Page</h1>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda,
+        at
+        consequuntur delectus doloribus dolorum est explicabo hic illum inventore iure
+        magnam,
+        modi nostrum omnis recusandae similique veritatis voluptate voluptates?</p>
+
+    </div>
+  )
+    ;
+}
+
+export default Page;
---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,16 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events in London</h1>
+
+      <a href="">Event 1</a>
+      <a href="">Event 2</a>
+      <a href="">Event 3</a>
+      <a href="">Event 4</a>
+      <a href="">Event 5</a>
+      <a href="">Event 6</a>
+    </div>
+  );
+}
+
+export default Page;
